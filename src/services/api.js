e25b6fbe77d90678f3eabc6c7c73960a819a8edd
Vlad <vladhopilo@gmail.com>
2024-06-27 09:34:19 +0300

    api request done

--- src/services/api.js
+++ src/services/api.js
@@ -1,23 +1,30 @@
 import axios from "axios";
 
 // const Authorization = process.env.REACT_APP_API_KEY;
-const Authorization = "Client-ID j_knc_osII1b4Vten-5gcRoQrwAYBXbrkLaadAh9fzY"
+const token ="Client-ID j_knc_osII1b4Vten-5gcRoQrwAYBXbrkLaadAh9fzY"
+const Authorization = "j_knc_osII1b4Vten-5gcRoQrwAYBXbrkLaadAh9fzY"
+
+// const setToken = token =>
+//     (axios.defaults.headers.common.Authorization = `Authorization ${token}`);
 
 const baseUrl = "https://api.unsplash.com/photos/";
 
-console.log(Authorization)
 
 export const getImages = async () => {
-console.log(Authorization)
+// console.log(Authorization)
   
     try {
-console.log(Authorization)
+// console.log(Authorization)
 
-      const response = await axios.post(baseUrl, {
-        headers:  Authorization
+      const response = await axios.get('https://api.unsplash.com/photos/',
+        {headers: {
+            Authorization: `Client-ID ${Authorization}`
+          }
     
     });
-      return response;
+    console.log(response)
+
+      return response.data;
     } catch (error) {
       console.error("Помилка", error);
       throw error;

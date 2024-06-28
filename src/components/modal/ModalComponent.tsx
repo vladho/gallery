4789d06c40b9c8e6b4a8753eed709c88cf85232c
Vlad <vladhopilo@gmail.com>
2024-06-28 12:05:56 +0300

    upd

---
+++ src/components/modal/ModalComponent.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { Box, Modal, Button, Typography } from '@mui/material';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+const ModalComponent = () => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div>
+      <Button onClick={handleOpen} variant="contained">Відкрити Модалку</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-title"
+        aria-describedby="modal-description"
+      >
+        <Box sx={style}>
+
+          <Button onClick={handleClose} variant="contained" sx={{ mt: 2 }}>Закрити</Button>
+        </Box>
+      </Modal>
+    </div>
+  );
+};
+
+export default ModalComponent;
